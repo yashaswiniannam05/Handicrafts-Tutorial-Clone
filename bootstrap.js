98366efe38e99aa36417abc8199e145728c624f0
Yashaswini Annam <annamyv@rknec.edu>
2025-06-22 12:51:27 +0530

    Intial Commit - Handicrafts Tutorial Clone

---
+++ bootstrap.js
@@ -0,0 +1,9 @@
+// let menuButton = document.querySelector(".menu_button");
+// console.log(menuButton);
+// let menuButtonAriaExpanded = menuButton.getAttribute("aria-expanded");
+// console.log(menuButtonAriaExpanded);
+// let navbar = document.querySelector(".nav");
+// console.log(navbar);
+// if (menuButtonAriaExpanded == false){
+//     navbar.style.height = "10em";
+// }
